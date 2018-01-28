19b7effb442618aadd13a9ae81073abd397f641d
MohmmadReza <karegar@gmail.com>
2018-01-28 03:49:52 +0330

    fixed another small bug

--- src/IntermediateCode/Instruction/Instruction.java
+++ src/IntermediateCode/Instruction/Instruction.java
@@ -16,10 +16,10 @@ public class Instruction {
                        InstructionParameter ip2,
                        InstructionParameter ip3) {
         this.type = type;
-        parameters = new InstructionParameter[4]; // TODO: 1/28/18 siez?
-        parameters[1] = ip1;
-        parameters[2] = ip2;
-        parameters[3] = ip3;
+        parameters = new InstructionParameter[3];
+        parameters[0] = ip1;
+        parameters[1] = ip2;
+        parameters[2] = ip3;
     }
 
     public InstructionType getType() {
